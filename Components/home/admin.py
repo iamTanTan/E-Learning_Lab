01154e9f3a1d4dbb8bee4764a74ad9f8bd975a22
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-03-25 19:08:12 -0700

    add basic home notifications

--- Components/home/admin.py
+++ Components/home/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 from .models import HomeNotification
 
 # Register your models here.
-admin.site.register(HomeNotification)
+class HomeNotificationAdmin(admin.ModelAdmin):
+    list_display = ('message', 'active')
+    list_filter = ['message', 'active']
+    fieldsets = (
+        ('Notification content', {
+            'fields': ['message']
+        }),
+        ('Show on HomePage', {
+            'fields': ['active']
+        }),
+    )
+
+admin.site.register(HomeNotification, HomeNotificationAdmin)
--- Components/home/models.py
+++ Components/home/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class HomeNotification(models.Model):
     message = models.TextField(max_length = 200)
     active = models.BooleanField(default=True)
+
+    class Meta: 
+        # Add verbose name 
+        verbose_name = 'Home Page Notification'
