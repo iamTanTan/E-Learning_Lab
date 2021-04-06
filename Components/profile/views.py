2372e8e76e189e029c0a3a2a50eaf7f84222d13c
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-04-06 14:49:08 -0700

    in progress add enrolled courses

--- Components/profile/views.py
+++ Components/profile/views.py
@@ -7,9 +7,13 @@ from .models import Profile
 def profile_detail(request):
 
     profile = Profile.objects.filter(user = request.user)
+    enrolled_courses = request.user.courses.all()
+
+    # request.user.courses.add('f1fc85d1-351e-4bf8-90ab-313367bd1d1c')
 
     context = {
         'profile': profile,
+        'enrolled_courses': enrolled_courses,
     }
 
     return render(request,"profile_detail.html", context)
