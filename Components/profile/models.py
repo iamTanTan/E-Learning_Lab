f70b81afed0954e0709b6c1ecb54b22f215ca711
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-04-08 21:41:09 -0700

    fix profile creation issue on login

--- Components/profile/models.py
+++ Components/profile/models.py
@@ -12,16 +12,27 @@ class Profile(models.Model):
         (TEACHER, 'Teacher'),
         (ADMINISTRATOR, 'Administrator'),
     )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=30, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    location = models.CharField(max_length=30, blank=True, null=True)
     birthdate = models.DateField(null=True, blank=True)
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):  # __unicode__ for Python 2
         return self.user.username
 
+     # returns the url to 'profile_detail.html' 
+    def get_absolute_url(self):
+        return reverse("profile_detail")  
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        print('new user and profile added')
+
+
+    Profile.objects.get_or_create(user=instance)
     instance.profile.save()
+
