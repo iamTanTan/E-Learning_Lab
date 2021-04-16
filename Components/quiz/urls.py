8d660bcfe19ed8bf6439c075507f0b98a86f1fea
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-04-16 00:13:36 -0700

    update regex urls to use re_path

--- Components/quiz/urls.py
+++ Components/quiz/urls.py
@@ -1,8 +1,4 @@
-try:
-    from django.conf.urls import url
-except ImportError:
-    from django.urls import re_path as url
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
@@ -11,32 +7,32 @@ urlpatterns = [
 
     path('<course_id>/gradebook/', views.viewQuizScoresByCourse, name='course_progress'),
 
-    path(r'^category/$',
+    re_path(r'^category/$',
         views.CategoriesListView.as_view(),
         name='quiz_category_list_all'),
 
-    path(r'^category/(?P<category_name>[\w|\W-]+)/$',
+    re_path(r'^category/(?P<category_name>[\w|\W-]+)/$',
         views.ViewQuizListByCategory.as_view(),
         name='quiz_category_list_matching'),
 
-    path(r'^progress/$',
+    re_path(r'^progress/$',
         views.QuizUserProgressView.as_view(),
         name='quiz_progress'),
 
-    path(r'^marking/$',
+    re_path(r'^marking/$',
         views.QuizMarkingList.as_view(),
         name='quiz_marking'),
 
-    path(r'^marking/(?P<pk>[\d.]+)/$',
+    re_path(r'^marking/(?P<pk>[\d.]+)/$',
         views.QuizMarkingDetail.as_view(),
         name='quiz_marking_detail'),
 
     #  passes variable 'quiz_name' to quiz_take view
-    path(r'^(?P<slug>[\w-]+)/$',
+    re_path(r'^(?P<slug>[\w-]+)/$',
         views.QuizDetailView.as_view(),
         name='quiz_start_page'),
 
-    path(r'^(?P<quiz_name>[\w-]+)/take/$',
+    re_path(r'^(?P<quiz_name>[\w-]+)/take/$',
         views.QuizTake.as_view(),
         name='quiz_question'),
 ]
