c2bedab928401faf22b1b27c99eff680be4edd2b
iamTanTan <70108451+iamTanTan@users.noreply.github.com>
2021-08-22 08:10:00 -0700

    remove unused imports

--- Components/home/forms.py
+++ Components/home/forms.py
@@ -1,8 +1,6 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.core.exceptions import ValidationError
- 
  
 class CustomUserCreationForm(forms.Form):
     username = forms.CharField(label='Enter Username', min_length=4, max_length=150)
--- Components/home/views.py
+++ Components/home/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.views import View
-
-
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-
 from .forms import CustomUserCreationForm
 from .models import HomeNotification
 
